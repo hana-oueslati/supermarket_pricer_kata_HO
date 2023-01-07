27f57a7c87e4670d63acb98ad668187d1661d10d
hana-oueslati <hana.oueslati1@gmail.com>
2023-01-07 12:15:56 +0100

    item class implemented

--- src/main/java/org/supermarket/domain/Item.java
+++ src/main/java/org/supermarket/domain/Item.java
@@ -1,11 +1,16 @@
 package org.supermarket.domain;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class Item {
     private String name;
     private float price;
 
     public Item(String name, float price) {
-        this.name = name;
+        if (StringUtils.isBlank(name))
+            throw new IllegalArgumentException("Item name should not be null, empty or whitespace");
+        else
+            this.name = name;
         this.price = price;
     }
 }
